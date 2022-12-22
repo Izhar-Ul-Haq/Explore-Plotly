f059f17a0d42f93c4e3fd3b24f4c08cf9d24c5e6
Internee <Internee@LRO-SH-21.vu.edu.pk>
2022-12-22 12:56:41 +0500

    Symbol is added

--- sizeAndColor.py
+++ sizeAndColor.py
@@ -7,3 +7,7 @@ fig.show()
 fig = px.scatter(df, x="sepal_width", y="sepal_length", color='species',
                  size='petal_length', hover_data=['petal_width'])
 fig.show()
+import plotly.express as px
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", symbol="species")
+fig.show()
